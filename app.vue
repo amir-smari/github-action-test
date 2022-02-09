680a2b29c1170a4f015bf8b6b1dfaa7565c07e03
Achraf ABIDA <achraf.abida@tekab.dev>
2022-02-09 10:51:33 +0100

    DFP-460 fix not found files in build mode

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <Html>
       <Head>
         <Title>devfactory</Title>
-        <Link rel="icon" type="image/x-icon" href="/favicon .ico" />
+        <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
     </Html>
     <NuxtPage />
