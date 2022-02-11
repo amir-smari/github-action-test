4fdc20c87484682bbb1df85f370f4d027b58ac28
aymenIsfiaya <aymen.isfiaya@tekab.dev>
2022-02-11 10:44:58 +0000

    DFP464 add:viewer image

---
+++ plugins/v-viewer.client.js
@@ -0,0 +1,6 @@
+import { defineNuxtPlugin } from '#app'
+import VueViewer from 'v-viewer'
+import 'viewerjs/dist/viewer.css'
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueViewer)
+})
