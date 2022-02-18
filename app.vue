7b3416fd40aea5910adfcff13348be0d8f596b99
aymenIsfiaya <aymen.isfiaya@tekab.dev>
2022-02-18 11:43:42 +0000

    DBS-12 add: meta tag description

--- app.vue
+++ app.vue
@@ -2,7 +2,14 @@
   <div>
     <Html>
       <Head>
-        <Title>devfactory</Title>
+        <Title>DevFactory</Title>
+        <Meta
+          name="description"
+          content="`DevFactory.ai – Plateforme web d’automatisation des workflows. Déploiement de micro-services pour réduire le temps de production et le coût des développements web
+Environnements Javascript et Typescript – Rest – GraghQL – Backend – Frontend
+Paris – Sfax – Contactez-nous
+`"
+        />
         <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
     </Html>
