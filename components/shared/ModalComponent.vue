e5dca52b16981e0baf3c164c2238b1608c117515
aymenIsfiaya <aymen.isfiaya@tekab.dev>
2022-02-18 09:59:03 +0000

    DFP-491 add: modal video

--- components/shared/ModalComponent.vue
+++ components/shared/ModalComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="modal-overlay" @click="$emit('close-modal')">
-    <div class="modal">
+    <div class="modal" :class="fit ? 'fit-content' : ''">
       <slot></slot>
     </div>
     <div class="close"></div>
@@ -11,12 +11,14 @@
 .modal-overlay {
   position: fixed;
   top: 0;
-  bottom: 0;
   left: 0;
   right: 0;
-  display: flex;
-  justify-content: center;
-  background-color: #000000da;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  // display: flex;
+  // justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
   z-index: 2;
 }
 
@@ -29,5 +31,23 @@
   height: 95%;
   border-radius: 6px;
   z-index: 3;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.fit-content {
+  width: fit-content;
+  height: fit-content;
 }
 </style>
+<script>
+export default {
+  props: {
+    fit: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
