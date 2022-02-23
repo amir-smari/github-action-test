b7b37e8b4f6a3a171445d36050757851d0c51237
Achraf ABIDA <achraf.abida@tekab.dev>
2022-02-23 15:04:30 +0100

    DFP-496 improuve send mail

--- server/api/contact.ts
+++ server/api/contact.ts
@@ -4,45 +4,37 @@ import { createTransport, SendMailOptions } from "nodemailer";
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
   const { form } = await useBody(req);
-  console.log("EMAIL_HOST: ",process.env.EMAIL_HOST);
-  
-  console.log(form);
-  if (form.email && form.name && form.message && form.subject) {
-    const transporter = createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: true, // true for 465, false for other ports
-      logger: false,
-      debug: false,
-      secureConnection: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-      tls: {
-        rejectUnAuthorized: true,
-      },
-    });
-    //TODO set envirenment variable for smtp config
-    // TODO set content of the email
-    const mailOptions: SendMailOptions = {
-      from: process.env.SMTP_ADMIN_EMAIL,
-      to: process.env.CONTACT_FORM_SEND_TO,
-      subject: "Dev Factory Landing page contact Form",
-      html: `<p>${Object.keys(form).map(key => `
-        ${key} : ${form[key]} <br>
-      `)}</p>`,
-    };
-    transporter.sendMail(mailOptions, (err: any, data: any) => {
-      if (err) {
-        res.statusCode = 500
-        return console.log("Error occurs", err);
-      }
-      res.statusCode = 200
-      return "Email sent!!!";
-    });
-  } else {
+
+  if (!(form.email && form.name && form.message && form.subject)) {
     res.statusCode = 400;
-    return "error";
+    return "Invalid request Body";
+  }
+  const transporter = createTransport({
+    host: String(process.env.SMTP_HOST),
+    port: Number(process.env.SMTP_PORT),
+    secure: true, // true for 465, false for other ports
+    debug: false,
+    auth: {
+      user: String(process.env.SMTP_USER),
+      pass: String(process.env.SMTP_PASS),
+    },
+  });
+
+  const mailOptions: SendMailOptions = {
+    from: process.env.SMTP_ADMIN_EMAIL,
+    to: process.env.CONTACT_FORM_SEND_TO,
+    subject: "Dev Factory Landing page contact Form",
+    html: `<p>${Object.keys(form).map(
+      (key) => `
+        ${key} : ${form[key]} <br>
+      `
+    )}</p>`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    return "mail sent";
+  } catch (error) {
+    res.statusCode = 500;
+    return "An error occurred";
   }
 };
