c74f306ecf1b80ccfd84b007ac1a103fd32aea6b
Achraf ABIDA <achraf.abida@tekab.dev>
2022-02-23 12:29:20 +0100

    DFP-500 Fix google analitics integration

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,11 +1,13 @@
 import VueGtag from "vue-gtag-next";
-import { defineNuxtPlugin } from "#app";
+import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
+
   nuxtApp.vueApp.use(VueGtag, {
     property: {
       id: config.GA_MEASUREMENT_ID,
     },
+    useDebugger: true,
   });
 });
