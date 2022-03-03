ad3e4ddae312bb6a99951033a9a723d157dadc10
Achraf ABIDA <achraf.abida@tekab.dev>
2022-03-03 14:16:19 +0100

    update seo tags

--- app.vue
+++ app.vue
@@ -5,10 +5,9 @@
         <Title>DevFactory</Title>
         <Meta
           name="description"
-          content="`DevFactory.ai – Plateforme web d’automatisation des workflows. Déploiement de micro-services pour réduire le temps de production et le coût des développements web
+          content="DevFactory.ai – Plateforme web d’automatisation des workflows. Déploiement de micro-services pour réduire le temps de production et le coût des développements web
 Environnements Javascript et Typescript – Rest – GraghQL – Backend – Frontend
-Paris – Sfax – Contactez-nous
-`"
+Paris – Sfax – Contactez-nous"
         />
         <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
