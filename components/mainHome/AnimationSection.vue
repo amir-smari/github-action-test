ffb03cafdaa99fd7e0fdb5704f8dde18499c266f
aymenIsfiaya <aymen.isfiaya@tekab.dev>
2022-03-03 16:01:02 +0000

    DFP-510 change: youtube video link

--- components/mainHome/AnimationSection.vue
+++ components/mainHome/AnimationSection.vue
@@ -13,7 +13,7 @@
         <iframe
           width="100%"
           height="720"
-          src="https://www.youtube.com/embed/GrymbJRJ4CY"
+          src="https://www.youtube.com/embed/6B8uhpx9Ae8"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
